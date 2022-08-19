842cbbb43083b44ece33d4361aba317856d5fe8d
JoaoFelipe25 <joaofelipemarcelinodacruz@yahoo.com.br>
2022-08-19 11:45:36 -0300

    aula 115

---
+++ projeto/src/main/java/com/aula115/projeto/controllers/AuthController.java
@@ -0,0 +1,27 @@
+package com.aula115.projeto.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aula115.projeto.repository.RoleRepository;
+import com.aula115.projeto.repository.UserRepository;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    
+}
---
+++ projeto/src/main/java/com/aula115/projeto/models/ERole.java
@@ -0,0 +1,8 @@
+package com.aula115.projeto.models;
+
+public enum ERole {
+    
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN,
+}
